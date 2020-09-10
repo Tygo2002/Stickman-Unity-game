7297d5d9fc1947117822e86747d773a916fac88c
Tygo Geervliet <tygog@live.nl>
2020-09-10 10:54:28 +0200

    Networking+ cube player
    networking toegevoegd mirror
    player (cube)
    player movement + jump (cube)

---
+++ Assets/FaceMouseController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceMouseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            //deactive left arm script
+            GameObject.Find("Right").GetComponent<FaceMouse>().enabled = true;
+            GameObject.Find("Left").GetComponent<FaceMouse1>().enabled = false;
+           
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //deactive right arm script
+            GameObject.Find("Left").GetComponent<FaceMouse1>().enabled = true;
+            GameObject.Find("Right").GetComponent<FaceMouse>().enabled = false;
+        
+        }
+        else
+        {
+
+        }
+       
+    }
+}
---
+++ Assets/TurnOfRemotePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Mirror;
+
+public class TurnOfRemotePlayer : Mirror.NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement script = this.GetComponent<PlayerMovement>();
+       
+        if (isLocalPlayer)
+        {
+            script.enabled = true;
+        }
+        else
+        {
+            //remote player 
+            script.enabled = false;
+        }
+    }
+}
